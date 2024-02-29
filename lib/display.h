f7d8e55df973eb0d52413bfa6ac6bb0ed55d4b2f
Kucky1956 <willy.kuckelsberg@gmail.com>
2024-02-29 23:24:49 +0100

    TFT_eSPI insert

--- lib/display.h
+++ lib/display.h
@@ -12,6 +12,9 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_ILI9341.h>
 
+#include <TFT_eSPI.h> // Hardware-specific library
+#include <SPI.h>
+
 #include "def.h"
 
 #include "..\lib\model.h"
@@ -19,11 +22,19 @@
 #define LOCAL_DEBUG
 #include "myLogger.h"
 
+#define MENU_COL 20
+#define MENU_ROW 40
+#define ROW_GAB 20
+#define COL_GAB 120
+
+#define TFT_GREY 0x5AEB
+
 class Display : public Task::Base
 {
 
 protected:
-    Adafruit_ILI9341 *_tft;
+    // Adafruit_ILI9341 *_tft;
+    TFT_eSPI *_tft;
 
 private:
     model_t *_model;
@@ -50,26 +61,17 @@ public:
         _model = model;
         LOGGER_VERBOSE("....leave");
         return this;
-    } /*--------------------- end of setModel --------------------------------------------------*/
+    } /*--------------------- end of setModel ---------------------------------------------------*/
 
     virtual void begin() override
     {
-        Serial.println("begin");
-        _tft = new Adafruit_ILI9341(PIN_TFT_CS, PIN_TFT_DC, PIN_TFT_MOSI, PIN_TFT_CLK, PIN_TFT_RST, PIN_TFT_MISO);
+        LOGGER_VERBOSE("enter....");
+        static int menu_row = MENU_ROW;
+        // _tft = new Adafruit_ILI9341(PIN_TFT_CS, PIN_TFT_DC, PIN_TFT_MOSI, PIN_TFT_CLK, PIN_TFT_RST, PIN_TFT_MISO);
+        _tft = new TFT_eSPI();
+        _tft->init();
 
-        _tft->begin();
-
-        delay(100);
-        _tft->setRotation(2);
-        _tft->setCursor(30, 20);
-        _tft->fillScreen(ILI9341_LIGHTGREY);
-        _tft->setTextColor(ILI9341_RED);
-        _tft->setTextSize(4);
-        _tft->println("KuckyCopter begin");
-
-        delay(2000);
-
-    //    read diagnostics (optional but can help debug problems)
+        // read diagnostics (optional but can help debug problems)
         uint8_t x = _tft->readcommand8(ILI9341_RDMODE);
         Serial.print("Display Power Mode: 0x");
         Serial.println(x, HEX);
@@ -86,40 +88,50 @@ public:
         Serial.print("Self Diagnostic: 0x");
         Serial.println(x, HEX);
 
-        Serial.println("begin end");
-    } /*--------------------- end of begin -----------------------------------------------------*/
+        delay(2000);
+
+        _tft->fillScreen(TFT_GREY);
+
+        _tft->setTextColor(TFT_WHITE, TFT_GREY); // Adding a background colour erases previous text automatically
+
+        _tft->drawCentreString("Kucky Copter 2", COL_GAB, 10, 4);
+        _tft->setRotation(3);
+        _tft->begin();
+        _tft->setTextSize(2);
+        _tft->setTextColor(TFT_YELLOW);
+        _tft->drawString("YAW", MENU_COL, menu_row);
+        _tft->drawString("Pitch :", MENU_COL, menu_row += ROW_GAB);
+        _tft->drawString("Roll :", MENU_COL, menu_row += ROW_GAB);
+        _tft->drawString("Altitude :", MENU_COL, menu_row += ROW_GAB);
+        _tft->drawString("to ground :", MENU_COL, menu_row += ROW_GAB);
+        _tft->drawString("Battery :", MENU_COL, menu_row += ROW_GAB);
+
+        LOGGER_VERBOSE("....leave");
+    } /*--------------------- end of begin ------------------------------------------------------*/
 
     virtual void update() override
     {
-     //   showLogo();
-        // _tft->setCursor(0, 0);
-        // _tft->setTextColor(ILI9341_BLUE);
-        // _tft->setTextSize(1);
-        // _tft->println("KuckyCopter");
-        
-        // Serial.println("KuckyCopter");
-        // Serial3.println("KuckyCopter on BT");
+        LOGGER_VERBOSE("enter....");
+        static int menu_row = MENU_ROW;
 
-        // {
-        //     LOGGER_NOTICE("Display fly screen");
-        // }
+        _tft->setTextSize(2);
+        _tft->setTextColor(TFT_YELLOW); // show values
+        _tft->drawNumber(_model->RC_interface.TX_payload.rcThrottle, MENU_COL + COL_GAB, menu_row);
+        _tft->drawNumber(_model->RC_interface.TX_payload.rcYaw, MENU_COL + COL_GAB, menu_row += ROW_GAB);
+        _tft->drawNumber(_model->RC_interface.TX_payload.rcPitch, MENU_COL + COL_GAB, menu_row += ROW_GAB);
+        _tft->drawNumber(_model->RC_interface.TX_payload.rcRoll, MENU_COL + COL_GAB, menu_row += ROW_GAB);
+        _tft->drawNumber(_model->RC_interface.TX_payload.rcAltitudeBaroAdj, MENU_COL + COL_GAB, menu_row += ROW_GAB);
+        _tft->drawNumber(_model->RC_interface.TX_payload.rcAltitudeSonicAdj, MENU_COL + COL_GAB, menu_row += ROW_GAB);
 
+        _tft->setTextColor(TFT_GREY); // hide values
+        _tft->drawNumber(_model->RC_interface.TX_payload.rcThrottle, MENU_COL + COL_GAB, menu_row);
+        _tft->drawNumber(_model->RC_interface.TX_payload.rcYaw, MENU_COL + COL_GAB, menu_row += ROW_GAB);
+        _tft->drawNumber(_model->RC_interface.TX_payload.rcPitch, MENU_COL + COL_GAB, menu_row += ROW_GAB);
+        _tft->drawNumber(_model->RC_interface.TX_payload.rcRoll, MENU_COL + COL_GAB, menu_row += ROW_GAB);
+        _tft->drawNumber(_model->RC_interface.TX_payload.rcAltitudeBaroAdj, MENU_COL + COL_GAB, menu_row += ROW_GAB);
+        _tft->drawNumber(_model->RC_interface.TX_payload.rcAltitudeSonicAdj, MENU_COL + COL_GAB, menu_row += ROW_GAB);
+
+        LOGGER_VERBOSE("....leave");
     } /*--------------------- end of update -----------------------------------------------------*/
-
-void showLogo() {
-  _tft->fillScreen(ILI9341_BLACK);
- 
-  _tft->setCursor(0, 0);
-  _tft->setTextColor(ILI9341_WHITE);  _tft->setTextSize(3);
-  _tft->println("Kucky Copter");
-  _tft->setTextColor(ILI9341_YELLOW); _tft->setTextSize(2);
-  _tft->println();
-  _tft->println("YAW");
-  _tft->setTextColor(ILI9341_RED);    _tft->setTextSize(2);
-  _tft->println();
-  _tft->println("Pitch");
-  _tft->println();
-  _tft->println("Roll");
-}
 };
 /*------------------------- end of display class ------------------------------------------------*/
